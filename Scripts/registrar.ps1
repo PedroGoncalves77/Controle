4a55879d8b2e6786f0d91edb72636eecc3e4261c
PedroGoncalves77 <117955845+PedroGoncalves77@users.noreply.github.com>
2024-01-31 10:32:28 -0300

    Update registrar.ps1

--- Scripts/registrar.ps1
+++ Scripts/registrar.ps1
@@ -3,5 +3,5 @@ $younger = "$n.md"
 $old = "$n.md~"
 $pathOld = "C:\Projetos\ControleDeMaterial\Anot\$old"
 $pathYoung = "C:\Projetos\ControleDeMaterial\Anot\$younger"
-copy $pathYoung $PathOld  2>&1 > $null
-rm $pathOld 2>&1 > $null
+copy $pathYoung $PathOld  2>&1 >> error.log
+rm $pathOld 2>&1 >> error.log
